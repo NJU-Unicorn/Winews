057e9c8d482e51305056935f45fb7c26e1fe9d79
DengrongGuan12 <15950570277@163.com>
2015-01-31 12:18:40 +0800

    海南日报

---
+++ Winews/src/cn/nju/edu/winews/crawler/handler/impl/HainanrbHandler.java
@@ -0,0 +1,14 @@
+package cn.nju.edu.winews.crawler.handler.impl;
+
+import cn.nju.edu.winews.crawler.handler.WiHandler;
+
+public class HainanrbHandler extends WiHandler{
+	public HainanrbHandler(String sourceID) {
+		super("hainanrb");
+	}
+	
+	public HainanrbHandler() {
+		super("hainanrb");
+	}
+
+}
