aa6e6c1673888a842fc4b7bddd53c35e3f9d9049
DengrongGuan12 <15950570277@163.com>
2015-01-31 16:05:09 +0800

    青海

---
+++ Winews/src/cn/nju/edu/winews/crawler/handler/impl/QinghairbHandler.java
@@ -0,0 +1,14 @@
+package cn.nju.edu.winews.crawler.handler.impl;
+
+import cn.nju.edu.winews.crawler.handler.WiHandler;
+
+public class QinghairbHandler extends WiHandler{
+	public QinghairbHandler(String sourceID) {
+		super("qinghairb");
+	}
+	
+	public QinghairbHandler() {
+		super("qinghairb");
+	}
+
+}
