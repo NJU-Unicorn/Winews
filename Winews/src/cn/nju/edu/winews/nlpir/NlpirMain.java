b175aee1eefe9846b8d3efb467177f719f77c38b
wxppt <wx_ppt@126.com>
2015-02-04 21:32:59 +0800

    词库更新

--- Winews/src/cn/nju/edu/winews/nlpir/NlpirMain.java
+++ Winews/src/cn/nju/edu/winews/nlpir/NlpirMain.java
@@ -3,9 +3,8 @@ package cn.nju.edu.winews.nlpir;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Scanner;
-
-import javax.swing.JOptionPane;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import cn.nju.edu.winews.crawler.entity.WiNews;
 import cn.nju.edu.winews.data.MongoHelper;
@@ -29,22 +28,41 @@ public class NlpirMain {
 		while (cur.hasNext()) { // 如果有下一条
 			DBObject obj = cur.next(); // 取得新闻
 			WiNews news = WiConvertor.DBObject2News(obj); // BSON格式数据转WiNews
-			if (news.getContent().equals("")) { // 如果正文为空则跳过
+			if (news.getContent().length()<30) { // 如果正文为空则跳过
 				continue;
 			}
+//			System.out.println(news.getUrl());
 			String titleNlpir = nlpir
 					.NLPIR_ParagraphProcess(news.getTitle(), 1);
 			String subTitleNlpir = nlpir.NLPIR_ParagraphProcess(
 					news.getSubTitle(), 1);
 			String contentNlpir = nlpir.NLPIR_ParagraphProcess(
 					news.getContent(), 1);
-			System.out.println();
-			System.out.println("title: " + titleNlpir + "\nsub: " + subTitleNlpir + "\ncontent: "
-					+ contentNlpir); // 打印分词结果
-			System.out.println("----------------------------------------------------------------");
-			System.in.read();
-			System.in.read();
-			readDictFile(nlpir);
+			int totalWordsCnt = 0;
+			int govWordsCnt = 0;
+			Pattern p = Pattern.compile("/");
+			Matcher m = p.matcher(contentNlpir);
+			while (m.find()) {
+				totalWordsCnt++;
+			}
+			Pattern p2 = Pattern.compile("/gov");
+			Matcher m2 = p2.matcher(contentNlpir);
+			while (m2.find()) {
+				govWordsCnt++;
+			}
+//			System.out.println();
+//			System.out.println("title: " + titleNlpir + "\nsub: "
+//					+ subTitleNlpir + "\ncontent: " + contentNlpir); // 打印分词结果
+			if(((double)govWordsCnt)/((double)totalWordsCnt)>0.03) {
+				System.out.println(news.getTitle());
+			} else {
+				System.out.print(".");
+			}
+//			System.out
+//					.println("----------------------------------------------------------------");
+//			System.in.read();
+//			System.in.read();
+//			readDictFile(nlpir);
 		}
 	}
 
